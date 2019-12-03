e08f66944d20cd32c00d3ceec236de6fef6a65e9
Jonathan Mah <jon@jonmah.com>
2019-12-02 17:25:04 -0800

    Tested topstories call and got some data back

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getTopStoryIds } from './api/hacker-news';
 
-const App = () => <p>Hello</p>;
+const App = () => {
+  const [topStoryIds, setTopStoryIds] = useState([]);
+  useEffect(() => {
+    // setTopStoryIds("fuck didn't work");
+    getTopStoryIds().then(topStoryIds => setTopStoryIds(topStoryIds.data));
+  }, []);
+  return <div>{JSON.stringify(topStoryIds)}</div>;
+};
 
 export default App;
